35760313303c3024bfb1146727bc5430e4258dc8
Joshua Akangah <akangah89@gmail.com>
2024-02-21 13:57:47 +0000

    chore: add new icons

--- components/SVG/16x16.tsx
+++ components/SVG/16x16.tsx
@@ -13,3 +13,17 @@ export function ChevronRightIcon(props: SvgProps) {
 		</Svg>
 	);
 }
+
+export function ChevronDownIcon(props: SvgProps) {
+	return (
+		<Svg width="16" height="16" viewBox="0 0 16 16" fill="none" {...props}>
+			<Path
+				d="M4 6L8 10L12 6"
+				stroke={props.stroke}
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+		</Svg>
+	);
+}
