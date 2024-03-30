962b1229f960874515957ccde5b1cf5b54bdbfa8
Joshua Akangah <akangah89@gmail.com>
2024-03-30 22:13:33 +0000

    feat(wip): continue on accounts

--- components/Accounts/molecules/AccountsAccordion.tsx
+++ components/Accounts/molecules/AccountsAccordion.tsx
@@ -1,13 +1,13 @@
-import { Text, View } from "@/components/Shared/styled";
-import AccountCard from "./AccountCard";
+import { View } from "@/components/Shared/styled";
 import { dummyAccountData } from "../constants";
+import AccountCard from "./AccountCard";
 
 export default function AccountsAccordion() {
 	return (
 		<View className="flex flex-col space-y-5">
 			{dummyAccountData.map((account, index) => (
 				<View>
-					<AccountCard {...account} />
+					<AccountCard {...account} key={index} />
 				</View>
 			))}
 		</View>
