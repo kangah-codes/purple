5c0b4a63bef86127bade84385ddea965893a485b
Joshua Akangah <akangah89@gmail.com>
2024-08-23 15:59:56 +0000

    feat: add worker to clean up sessions

--- nucleus/cmd/api/main.go
+++ nucleus/cmd/api/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
 	"log"
+	"nucleus/cmd/workers"
 	"nucleus/internal/api/middleware"
 	"nucleus/internal/api/routes"
 	"nucleus/internal/api/types"
+	"nucleus/internal/models"
 	"nucleus/utils"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"time"
 
@@ -15,6 +21,7 @@ import (
 )
 
 func main() {
+	// load env variables
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
@@ -24,11 +31,18 @@ func main() {
 	utils.InitDB(dsn)
 	utils.InitLogger()
 
+	// get db instance
 	db := utils.GetDB()
-
 	if db == nil {
 		log.Fatal("Failed to connect to the database")
 	}
+
+	// context for workers
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// session cleaners
+	cleaner := workers.NewSessionCleaner(db)
+	cleaner.Start(ctx)
 
 	// rate limiting
 	rateLimitStore := ratelimit.InMemoryStore(&ratelimit.InMemoryOptions{
@@ -45,12 +59,13 @@ func main() {
 		},
 	})
 
-	// models.Migrate(db)
+	models.Migrate(db)
 	r := gin.Default()
 	r.Use(rateLimit)
 	r.Use(middleware.CorsMiddleware())
 	r.Use(middleware.APIKeyMiddleware())
 
+	// create api group
 	v1 := utils.CreateV1Group(r)
 	routes.RegisterUserRoutes(v1)
 	routes.RegisterAuthRoutes(v1)
@@ -64,10 +79,22 @@ func main() {
 		})
 	})
 
-	log.Printf("Starting server on port 8080")
+	utils.InfoLogger.Printf("Starting server on port 8080")
+
+	// setup graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-quit
+		utils.InfoLogger.Println("Shutting down server")
+
+		// stop workers
+		cancel()
+		cleaner.Stop()
+	}()
 
 	// Run the server on the specified port
 	if err := r.Run(":8080"); err != nil {
-		log.Fatalf("Failed to run server: %v", err)
+		utils.InfoLogger.Fatalf("Failed to run server: %v", err)
 	}
 }
