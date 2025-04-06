4d50b67bbb919cc7f3119501353fd99c1e927a15
Joshua Akangah <akangah89@gmail.com>
2025-04-06 00:09:59 +0000

    feat: more stuff

--- orbit/src/components/shared/Nav.tsx
+++ orbit/src/components/shared/Nav.tsx
@@ -1,5 +1,7 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react';
 
 export default function Nav() {
     return (
@@ -22,15 +24,6 @@ export default function Nav() {
                             <a
                                 className='flex items-center px-4 py-3 text-base font-medium text-gray-950 bg-blend-multiply data-hover:bg-black/[2.5%]'
                                 data-headlessui-state=''
-                                href='/features'
-                            >
-                                Features
-                            </a>
-                        </div>
-                        <div className='flex group/item relative'>
-                            <a
-                                className='flex items-center px-4 py-3 text-base font-medium text-gray-950 bg-blend-multiply data-hover:bg-black/[2.5%]'
-                                data-headlessui-state=''
                                 href='/pricing'
                             >
                                 Pricing
@@ -46,7 +39,7 @@ export default function Nav() {
                             </a>
                         </div>
                     </nav>
-                    <button
+                    {/* <button
                         className='flex size-12 items-center justify-center self-center rounded-lg data-hover:bg-black/5 lg:hidden'
                         aria-label='Open main menu'
                         id='headlessui-disclosure-button-:rl:'
@@ -54,21 +47,72 @@ export default function Nav() {
                         aria-expanded='false'
                         data-headlessui-state=''
                     >
-                        <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            viewBox='0 0 24 24'
-                            fill='currentColor'
-                            aria-hidden='true'
-                            data-slot='icon'
-                            className='size-6'
-                        >
-                            <path
-                                fillRule='evenodd'
-                                d='M3 9a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 9Zm0 6.75a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z'
-                                clipRule='evenodd'
-                            />
-                        </svg>
-                    </button>
+
+                    </button> */}
+                    <div className='flex size-12 items-center justify-center self-center rounded-lg data-hover:bg-black/5 lg:hidden'>
+                        <div className='flex gap-8'>
+                            <Popover>
+                                <PopoverButton className='block text-sm/6 font-semibold text-black focus:outline-none data-[active]:text-black data-[hover]:text-black data-[focus]:outline-1 data-[focus]:outline-white'>
+                                    <svg
+                                        xmlns='http://www.w3.org/2000/svg'
+                                        viewBox='0 0 24 24'
+                                        fill='currentColor'
+                                        aria-hidden='true'
+                                        data-slot='icon'
+                                        className='size-6'
+                                    >
+                                        <path
+                                            fillRule='evenodd'
+                                            d='M3 9a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 9Zm0 6.75a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z'
+                                            clipRule='evenodd'
+                                        />
+                                    </svg>
+                                </PopoverButton>
+                                <PopoverPanel
+                                    transition
+                                    anchor='bottom'
+                                    className={`
+                                        mt-5 w-full z-10 px-5 transition duration-200 ease-in-out [--anchor-gap:var(--spacing-5)]
+                                        data-[closed]:-translate-y-1 data-[closed]:opacity-0
+                                    `}
+                                >
+                                    {({ close }) => (
+                                        <div
+                                            className={`
+                                                p-5 bg-gradient-to-br from-purple-50 to-purple-100 divide-y divide-white/5 rounded-[3rem]
+                                                h-full w-full z-10 text-sm/6
+                                              `}
+                                        >
+                                            <Link
+                                                onMouseUp={() => close()}
+                                                className='block py-2.5 px-3.5 transition hover:bg-purple-200 rounded-3xl'
+                                                href='/pricing'
+                                            >
+                                                <p className='font-bold text-lg text-black'>
+                                                    Pricing
+                                                </p>
+                                                <p className='text-black'>
+                                                    Measure actions your users take
+                                                </p>
+                                            </Link>
+                                            <Link
+                                                onMouseUp={() => close()}
+                                                className='block py-2.5 px-3.5 transition hover:bg-purple-200 rounded-3xl'
+                                                href='/about'
+                                            >
+                                                <p className='font-bold text-lg text-black'>
+                                                    About Purple
+                                                </p>
+                                                <p className='text-black'>
+                                                    Create your own targeted content
+                                                </p>
+                                            </Link>
+                                        </div>
+                                    )}
+                                </PopoverPanel>
+                            </Popover>
+                        </div>
+                    </div>
                 </div>
             </header>
         </div>
